0baad87229ac33ed282a3aa45a13ae30cc66574e
Thomas Auinger <thomas@auinger.de>
2026-07-08 13:45:30 +0200

    synced 025-exrc

--- EXERCISES.md
+++ EXERCISES.md
@@ -1,15 +1,52 @@
-# Übungen zu 013 - XML Beans Container
+# Übungen zu 025 "Beans"
 
-Migrieren Sie die Pizza Anwendung auf den "Summer **XML** Bean-Container".
+Machen Sie aus der bestehenden Kommandozeilenanwendung eine Spring Boot basierte Anwendung.
 
-Das heißt, die Bean-Definition erfolgen nun in einer separaten XML-Datei, welche folgendes Format aufweist:
+Die bestehende Klasse `PizzaApp` ist hierfür der Ausgangspunkt. Diese wurde auf Basis einer bereits angepassten
+`pom.xml` bzw. `build.gradle` Datei bereits für Spring vorbereitet.
 
-````xml
-<?xml version="1.0" encoding="UTF-8" ?>
-<beans>
-    <bean name="the-name" class="de.example.the.Class"/>
-</beans>
-````
+Diese Übung besteht aus **zwei Phasen**:
 
-**Hinweis:** Wenn eine innere statische Klasse per Reflection geladen werden soll, so muss ein `$` Zeichen für die
-Trennung von äußerem zu innerem Klassennamen genutzt werden (`DataLoader$Sample`).
+## Phase 1
+
+Wir wollen, dass Spring nun alle Beans erzeugt und verwaltet.
+
+D.h. das Erzeugen der Instanzen überlassen wir ab jetzt Spring -- und machen dies nicht mehr
+mittels `new` selbst.
+
+Das heißt aber auch, dass Sie Spring darüber "informieren" müssen, welche Beans anzulegen sind.
+Dafür haben wir die notwendigen Annotationen kennengelernt.
+
+Gerne können Sie statt der komplexeren `JdbcProductRepository` Klasse die einfache `HashMapProductRepository` Variante
+nehmen.
+
+### Bonus: Nutzung Datenbank
+
+Wer die Datenbank-Variante des `ProductRepository` nutzen möchte, braucht hierfür eine laufende
+Datenbank und eine `DataSource` Bean. Diese legt Spring Boot für uns automatisch an, da wir die H2 Datenbank
+im Classpath haben (Autoconfiguration).
+
+Somit können eine ganze Reihe an eigenen Klassen entfernt werden:
+
+* `H2TcpServer.java` -- denn Spring startet automatisch eine interne H2 Datenbank
+* `SchemaScriptRunner.java` -- denn Spring führt automatisch eine vorhandene `schema.sql` Datei aus
+* `PersistenceException.java` -- das Spring JDBC Projekt bringt eine Reihe an Exceptions mit
+
+Die Nutzung des `JdbcProductRepository` ist dann gar nicht mehr so schwer.
+
+## Phase 2
+
+Wie arbeiten wir nun mit den jetzt vorhandenen Service-Beans?
+
+Sprich, an welcher Stelle erfolgt nun das Ausführen der Geschäftslogik, die bisher in der `PizzaApp.main()` Methode
+gestanden hat?
+
+Sie können eine der folgenden zwei Optionen nutzen:
+
+1. eine `LogicRunner` Klasse anlegen (implementiert `CommandLineRunner` oder `ApplicationRunner`)
+   - zur Spring Bean deklarieren
+   - Geschäftslogik hierhin kopieren
+   - notwendige Beans autowiren
+2. oder Sie nutzen den von `SpringApplication.run()` zurückgegebenen Kontext und holen sich von dort
+   die notwendigen Beans 
+
--- src/main/java/pizza/PizzaApp.java
+++ src/main/java/pizza/PizzaApp.java
@@ -3,30 +3,36 @@ package pizza;
 import com.github.freva.asciitable.AsciiTable;
 import com.github.freva.asciitable.Column;
 import com.github.freva.asciitable.HorizontalAlign;
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pizza.customer.Customer;
 import pizza.customer.CustomerService;
 import pizza.order.OrderService;
+import pizza.product.JdbcProductRepository;
 import pizza.product.Product;
 import pizza.product.ProductService;
-import summer.BeanContainer;
-import summer.XmlBeanContainer;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+@SpringBootApplication
 public class PizzaApp {
 
     public static void main(String[] args) {
-        // Instantiate beans ---
-        BeanContainer beanContainer = new XmlBeanContainer("/beans.xml");
+        // start Spring context
+        SpringApplication.run(PizzaApp.class, args);
 
-        // query and use beans
-        beanContainer.getBean(DataLoader.class).run();
-        ProductService productService = beanContainer.getBean(ProductService.class);
-        CustomerService customerService = beanContainer.getBean(CustomerService.class);
-        OrderService orderService = beanContainer.getBean(OrderService.class);
+        // Instantiate beans ---
+        H2TcpServer h2TcpServer = startDatabase();
+        DataSource dataSource = createDataSource();
+        ProductService productService = new ProductService(new JdbcProductRepository(dataSource));
+        CustomerService customerService = new CustomerService();
+        OrderService orderService = new OrderService(customerService, productService);
+        new DataLoader.Sample(productService, customerService).run();
 
         // Work with the data:
         var products = toList(productService.getAllProducts());
@@ -61,6 +67,33 @@ public class PizzaApp {
                     new Column().header("Est. Delivery").headerAlign(HorizontalAlign.LEFT).dataAlign(HorizontalAlign.LEFT).with(o -> o.getEstimatedTimeOfDelivery().toLocalTime().toString())
             )));
         }
+
+        // Stop database to make app terminate ---
+        stopDatabase(h2TcpServer);
+    }
+
+    static H2TcpServer startDatabase() {
+        H2TcpServer h2TcpServer = new H2TcpServer();
+        h2TcpServer.start();
+        return h2TcpServer;
+    }
+
+    static void stopDatabase(H2TcpServer h2TcpServer) {
+        if (h2TcpServer != null) {
+            h2TcpServer.stop();
+        }
+    }
+
+    static DataSource createDataSource() {
+        // use a H2 DataSource implementation
+        var dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:tcp://localhost:9092/~/training.spring.pizza");
+
+        // run a script to set up the database schema (=tables)
+        new SchemaScriptRunner(dataSource).run();
+
+        // return the data source for others to work with
+        return dataSource;
     }
 
     static <T> List<T> toList(Iterable<T> iterable) {
@@ -68,5 +101,4 @@ public class PizzaApp {
         iterable.forEach(list::add);
         return list;
     }
-
 }
